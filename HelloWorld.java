e97dca2f93de111c14e372841f786536edcaf064
LAPTOP-32FPR937\lc <739275625@qq.com>
2021-03-03 21:13:23 +0800

    变量

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+// 类名要和文件名一致
+public class HelloWorld {
+    // 程序的入口
+    public static void main(String[] args) {
+        System.out.println("hello,world!");
+    }
+}
---
+++ Variable/Variable.java
@@ -0,0 +1,13 @@
+public class Variable {
+    public static void main(String[] args) {
+        // long类型要加L
+        long num2 = 300000000000L;
+        /*
+        浮点数也要加
+        double 不用D
+         */
+        float num3 = 2.5F;
+        double num4 = 2.2; //
+        System.out.println(num3);
+    }
+}
